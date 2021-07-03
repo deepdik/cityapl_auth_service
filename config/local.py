44433ebbccde5f0ef074401181084a23e9dcf57b
Deepak Kumar <deepak.kumar@finoit.co.in>
2021-07-03 21:17:26 +0530

    added local.py

---
+++ config/local.py
@@ -0,0 +1,150 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'u*mvv8s^0&bu8#fact-k#g7rh-rz5!izrtbrlnii-o!+n$jjp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+API_ROOT = DEBUG # enable root api view
+
+SITE_URL = ''
+APP_LOGO = ''
+
+############################# Database #############################
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# mysql --host=cityapl-database-1.cyvpsljpkc8d.ap-south-1.rds.amazonaws.com --port=3306 --user=admin --password=cityapl_rds123
+
+
+# MYSQL RDS
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'cityapl',
+# 		'USER': 'admin',
+# 		'PASSWORD': 'cityapl_rds123',
+# 		'HOST': 'cityapl-database-1.cyvpsljpkc8d.ap-south-1.rds.amazonaws.com',
+# 		'PORT': '3306',
+# 	}
+# }
+
+##local Postgres database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cityapl',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+#  Server Postgresql
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'cityapl',
+#         'USER': 'myprojectuser',
+#         'PASSWORD': 'password',
+#         'HOST': '13.233.128.251',
+#         'PORT': '5432',
+#     }
+# }
+
+
+########################## DRF Settings #############################
+REST_FRAMEWORK = {
+	'DEFAULT_RENDERER_CLASSES': (
+		'rest_framework.renderers.JSONRenderer',
+		'rest_framework.renderers.BrowsableAPIRenderer',
+		# 'rest_framework.renderers.AdminRenderer',
+	),
+	'DEFAULT_AUTHENTICATION_CLASSES': (
+		'rest_framework.authentication.TokenAuthentication',
+		'rest_framework.authentication.SessionAuthentication',
+		'rest_framework.authentication.BasicAuthentication',
+	),
+	'DEFAULT_FILTER_BACKENDS': (
+		'django_filters.rest_framework.DjangoFilterBackend',
+		'rest_framework.filters.SearchFilter',
+		'rest_framework.filters.OrderingFilter',
+	),
+	'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
+	'DEFAULT_VERSION': 'v1',
+	'ALLOWED_VERSIONS': ('v1', 'v2'),
+	'DEFAULT_PAGINATION_CLASS': 'cityapl.pagination.StandardResultsPageNumberPagination',
+	'EXCEPTION_HANDLER': 'cityapl.exceptions.custom_exception_handler',
+}
+
+############################# Static ###############################
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, "cityapl/static"),
+	#'/var/project/static/',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+############################# Logging ###############################
+PY_LOGGING = DEBUG  # True/False
+ADMINS = (('Admin', ''),)
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_true': {
+			'()': 'django.utils.log.RequireDebugTrue'
+		},
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse',
+		}
+	},
+	'handlers': {
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': [
+				'require_debug_false',
+				# 'require_debug_true'
+			],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django.request': {
+			'handlers': ['mail_admins'],
+			'level': 'ERROR',
+			'propagate': True
+		}
+	}
+}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+SOCIAL_AUTH_GOOGLE_OAUTH_KEY = '961513181020-ll6lp4qe015ufmgi6udqk4q6qmul53fj.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH_SECRET = 'CoFFeTOJNoDgQxMJAJ2BcdGc'
